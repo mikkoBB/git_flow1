ff08f507b0b3ba7f3c2a463499e5afa751c5f441
mikhail izotov <157229582+mikkoBB@users.noreply.github.com>
2024-01-29 15:41:12 +0300

    func2

--- str_func.py
+++ str_func.py
@@ -1,7 +1,16 @@
 def upp_1():
     """
-    Делает все буквы заглавными
+    Делает буквы заглавными в каждом слове
     """
     user_input = input()
     print(user_input.upper())
 upp_1()
+
+
+def tit_1():
+    """
+    Делает первые буквы заглавными
+    """
+    user_input = input()
+    print(user_input.title())
+tit_1()
